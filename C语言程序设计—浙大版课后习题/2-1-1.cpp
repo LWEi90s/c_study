7e23a4f40979d47d37dcf3b60f74af5c844e9906
lwei <96482955+LWEi90s@users.noreply.github.com>
2022-04-30 22:14:23 +0800

    Add files via upload

---
+++ C语言程序设计—浙大版课后习题/2-1-1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,d,Sum,Average;
+	scanf(" %d %d %d %d",a,b,c,d);
+	Sum=(a+b+c+d);
+	Average=(a+b+c+d)/4;
+	printf("%d\n%d\n",Sum,Average);
+	
+	return 0;
+}
--- C语言程序设计—浙大版课后习题/2-17.cpp
+++ C语言程序设计—浙大版课后习题/2-17.cpp
@@ -1,7 +1,13 @@
-#include<stdint.h>
-int main(){
-	
-	
-	
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int n;
+	printf("请输入一个非负整数n\n");
+	scanf("%d",&n);
+	for(int i=0;i<=n;i++){
+		int j=pow(3,i);
+		printf("pow(3,%d) = %d\n",i,j);
+	}
 	return 0;
 }
